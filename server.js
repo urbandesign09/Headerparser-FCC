29e3566885283559544bec5fedfcd5b1138e6a72
Edbert Cheng <Edbertcheng@gmail.com>
2021-01-08 23:03:15 -0600

    commit

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//project problem
+app.get("/api/whoami", function(req, res) {
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']})
+});
 
 
 // listen for requests :)
